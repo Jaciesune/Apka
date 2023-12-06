22b0798ad6e0831a320fc0364b1c8c5f84f3432c
Jakub Piwko <jakub.piwko@op.pl>
2023-12-06 20:33:11 +0100

    ExploreX | Better Looking Theme Change

--- ExploreX/app/src/main/java/com/example/explorex/MainActivity.java
+++ ExploreX/app/src/main/java/com/example/explorex/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.explorex;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Interpolator;
+import android.view.animation.PathInterpolator;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +22,9 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.view.animation.DecelerateInterpolator;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -29,6 +37,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor lightSensor;
     private boolean isNightMode = false;
 
+    // Globalne zmienne kolorów
+    private int currentColor;
+    private int targetColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +84,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (lightSensor != null) {
             setupLightSensor();
         }
+
+        // Inicjalizacja globalnych zmiennych kolorów
+        currentColor = getResources().getColor(R.color.lightModeColorPrimary);
+        targetColor = getResources().getColor(R.color.lightModeColorPrimary);
     }
 
     // Menu
@@ -90,6 +105,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             logoutButton.setVisibility(View.GONE);
         }
     }
+
     public Button getLogoutButton() {
         return logoutButton;
     }
@@ -104,27 +120,69 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             float lightLevel = event.values[0];
 
             // Adjust the threshold based on your preference
-            float threshold = 10.0f;
+            float threshold = 25.0f;
 
+            int newColor;
             if (lightLevel < threshold && !isNightMode) {
                 // Enable NightMode
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 isNightMode = true;
+                targetColor = getResources().getColor(R.color.nightModeColorPrimary);
+                newColor = targetColor;
             } else if (lightLevel >= threshold && isNightMode) {
                 // Enable LightMode
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 isNightMode = false;
+                targetColor = getResources().getColor(R.color.lightModeColorPrimary);
+                newColor = targetColor;
+            } else {
+                // Use a custom easing function for smoother transition
+                float fraction = calculateFraction(lightLevel, threshold, 500.0f);
+                newColor = (int) new ArgbEvaluator().evaluate(fraction, currentColor, targetColor);
             }
+
+            // Animate the color change
+            animateColorChange(newColor);
         }
     }
+
+    // Custom easing function using PathInterpolator
+    private float calculateFraction(float x, float start, float end) {
+        // Map x to the range [0, 1]
+        float fraction = Math.max(0, Math.min(1, (x - start) / (end - start)));
+
+        // Use a PathInterpolator with custom control points
+        Interpolator interpolator = new PathInterpolator(0.4f, 0f, 0.2f, 1f);
+
+        // Apply the easing function
+        return interpolator.getInterpolation(fraction);
+    }
+
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // Do nothing here
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // Unregister the sensor listener to avoid memory leaks
         sensorManager.unregisterListener(this);
+    }
+
+    private void animateColorChange(int newColor) {
+        Drawable[] drawables = {
+                new ColorDrawable(currentColor),
+                new ColorDrawable(newColor)
+        };
+
+        TransitionDrawable transitionDrawable = new TransitionDrawable(drawables);
+        getWindow().setBackgroundDrawable(transitionDrawable);
+
+        transitionDrawable.startTransition(500); // Adjust the duration as needed
+
+        // Save the current color for the next iteration
+        currentColor = newColor;
     }
 }
